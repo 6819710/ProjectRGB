074e389f8a1bdd23cc20cebddc0c132ccdf98011
James Galovic <jpgalovic@gmail.com>
2020-02-01 03:11:36 +1100

    Add player horizontal movement, walls and ground

---
+++ ProjectRGB/Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float inputDirection;
+    private CharacterController controller;
+    private Vector3 moveVector;
+
+    public bool debug;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputDirection = Input.GetAxis("Horizontal");
+        if(debug)
+               Debug.Log(inputDirection);
+
+        moveVector = new Vector3(inputDirection / 10, 0, 0);
+
+        controller.Move(moveVector);
+    }
+}
