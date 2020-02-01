2e7b60eb1ea5be13e98c2972bde4e79620ae447d
James Galovic <jpgalovic@gmail.com>
2020-02-02 09:30:25 +1100

    Add coloured platform varients

--- ProjectRGB/Assets/Scripts/OnRed.cs
+++ ProjectRGB/Assets/Scripts/OnRed.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class OnRed : MonoBehaviour
 {
+    private GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (player.GetComponent<PlayerEquip>().equip_red)
+        {
+            GetComponent<Renderer>().enabled = true;
+            GetComponent<BoxCollider>().enabled = true;
+        }
+        else
+        {
+            GetComponent<Renderer>().enabled = false;
+            GetComponent<BoxCollider>().enabled = false;
+        }
     }
 }
