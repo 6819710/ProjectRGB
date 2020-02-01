b6930e0568b28b23e16c3d581467604871424e76
James Galovic <jpgalovic@gmail.com>
2020-02-01 11:11:02 +1100

    Add colour switching mechanic,
    Add same on green mechanic

---
+++ ProjectRGB/Assets/Scripts/OnGreen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnGreen : MonoBehaviour
+{
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.GetComponent<PlayerEquip>().equip_green)
+        {
+            GetComponent<Renderer>().enabled = true;
+            GetComponent<BoxCollider>().enabled = true;
+        }
+        else
+        {
+            GetComponent<Renderer>().enabled = false;
+            GetComponent<BoxCollider>().enabled = false;
+        }
+    }
+}
