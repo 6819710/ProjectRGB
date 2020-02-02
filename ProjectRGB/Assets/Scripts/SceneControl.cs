9801a469ce6344ae96c9a64160a41152a7057b98
James Galovic <jpgalovic@gmail.com>
2020-02-02 19:24:13 +1100

    various fixes

---
+++ ProjectRGB/Assets/Scripts/SceneControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneControl : MonoBehaviour
+{
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene("level");
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void LoadCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+}
